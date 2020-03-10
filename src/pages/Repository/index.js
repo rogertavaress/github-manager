2835f9ebee833b619929868ed3b8cc2e1eff824e
Rogerio Tavares <rogerioctf@gmail.com>
2020-03-10 08:48:21 -0300

    Carregando dados da API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import { Container } from '../Main/styles';
 
-export default function Repository({ match }) {
-    return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+import api from '../../services/api';
+
+export default class Repository extends Component {
+    state = {
+        repository: {},
+        issues: [],
+        loading: true,
+    };
+
+    async componentDidMount() {
+        const { match } = this.props;
+
+        const repoName = decodeURIComponent(match.params.repository);
+
+        const [repository, issues] = await Promise.all([
+            await api.get(`/repos/${repoName}`),
+            await api.get(`/repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5,
+                },
+            }),
+        ]);
+
+        this.setState({
+            loading: false,
+            repository: repository.data,
+            issues: issues.data,
+        });
+    }
+
+    render() {
+        const { loading, repository, issues } = this.state;
+
+        return (
+            <Container>
+                <h1>Repository:</h1>
+            </Container>
+        );
+    }
 }
